b1e35eff841264e4a60b665341ef0ac230e36982
Kristina Gromova <christina28romova@yandex.ru>
2021-04-17 15:04:48 +0300

    first page and card__room modul

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,59 @@
-import "../style/style.css"; 
+import "../pages/style.css"; 
+import "../components/utils/dropdown/dropdown";
 let someStr = 'Изменил, сохранил, собрал'; 
 console.log(someStr);
+
+
+//счетчик дропдауна
+const dropdownResult = document.getElementById('button-guest');
+
+const dropdownContainer = document.querySelector(".dropdown__items");
+
+dropdownResult.addEventListener('click', (e) => {
+    e.preventDefault();
+    dropdownContainer.classList.toggle('dropdown__items_open');
+    console.log('присвоить уникальный айдишник');
+});
+
+
+
+dropdownContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+    //контаинсом проверить где сработало событие
+    const counterButton = e.target.closest('.item__counter_button');
+const box = counterButton.parentElement;
+let inputHidden = box.lastElementChild.value;
+let arr = [];
+
+    const counterValue = box.querySelector('.item__counter_value');
+  
+    if (counterButton) {
+    
+      if (counterButton.value === '-') {
+        if (counterValue.textContent < 1) {
+            counterButton.previousElementSibling.disebled = true;
+           }
+          let count = counterValue.innerHTML--;
+          inputHidden = count;
+          //dropdownResult.textContent = `${count} гостя`
+          
+          return count
+      }
+     if (counterButton.value === '+') {
+      let count = counterValue.innerHTML++;
+      inputHidden = count;
+     
+      return count
+      }
+    
+    }
+    
+})
+
+
+$(document).on('click','.js-button__search', function(e){
+    e.preventDefault();
+
+    window.location.href='http://localhost:8080/room-details.html'
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const path = require('path');
+const webpack = require("webpack");
+const fs = require('fs');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключили плагин
-
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'assets/'
+  }
+const PAGES_DIR = `${PATHS.src}/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 module.exports = {
     entry: { 
-        main: './src/js/index.js',
-        search: './src/js/scripts/search.js'
+        app: PATHS.src,
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js'
+        filename: `${PATHS.assets}js/[name].[hash].js`,
+        path: PATHS.dist,
     },
+    
     module: {
         rules: [
+            {
+                test: /\.pug$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "pug-loader"
+                }
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -23,26 +39,45 @@ module.exports = {
     {
                 test: /\.css$/,
                 use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-             }
+             },
+             {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]'
+                }
+              }, {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]'
+                }
+              },
+              {
+                test: /\.json$/,
+                loader: 'json-loader'
+              }
         ]
     },
     plugins: [
-        
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+      }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+              preset: ['default'],
+            },
+            canPrint: true,
+          }),
         new MiniCssExtractPlugin({filename: 'style.css'}),
-        new HtmlWebpackPlugin({ // настроили плагин
-            inject: false,
-            hash: true,
-            template: './src/pages/index.pug',
-            filename: 'index.pug',
-            chunks: ['main'],
-        }),
-        new HtmlWebpackPlugin({ // настроили плагин
-            inject: false,
-            hash: true,
-            template: './src/pages/search.html',
-            filename: 'search.html',
-            chunks: ['search'],
-        })
+        
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+          }))
     ]
 };
     
